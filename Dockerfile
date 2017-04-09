69e7fd8bf39087997f4182f451620967c6b306bb
Guannan Du <nspforever@gmail.com>
2017-04-09 01:35:38 -0700

    add Dockerfile for building the tiny supervisord image

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+# https://medium.com/@kelseyhightower/optimizing-docker-images-for-static-binaries-b5696e26eb07
+FROM scratch
+MAINTAINER nspforever <nspforever@gmail.com>
+ADD supervisord supervisord
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"net/http"
+	"os"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/jessevdk/go-flags"
-	"os"
 )
 
 type Options struct {
 	Configuration string `short:"c" long:"configuration" description:"the configuration file" optional:"yes" default:"supervisord.conf"`
 }
 
+var (
+	client *http.Client
+	pool   *x509.CertPool
+)
+
 func init() {
+	pool = x509.NewCertPool()
+	pool.AppendCertsFromPEM(pemCerts)
+	client = &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{RootCAs: pool}}}
 	log.SetOutput(os.Stdout)
 	log.SetLevel(log.DebugLevel)
 }
